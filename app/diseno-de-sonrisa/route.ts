4878a2e843f7b740971c1aa60005cd7d7353582d
dracaromacareno-hue <dracaromacareno@gmail.com>
2026-07-22 16:22:48 -0500

    Create route.ts
    Route handler para remover noindex en diseño de sonrisa

---
+++ app/diseno-de-sonrisa/route.ts
@@ -0,0 +1,20 @@
+export async function GET() {
+  const response = await fetch(
+    'https://sites.leadconnectorhq.com/preview/oS37NiFZQpFc66t5Ts4L',
+    { cache: 'no-store' }
+  );
+
+  let html = await response.text();
+
+  html = html.replace(
+    /<meta[^>]*name=["']robots["'][^>]*content=["'][^"']*noindex[^"']*["'][^>]*>/gi,
+    ''
+  );
+
+  return new Response(html, {
+    status: response.status,
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+  });
+}
