5bfb3554fb4544d3a1b599f8666a599939645192
dracaromacareno-hue <dracaromacareno@gmail.com>
2026-07-22 16:33:53 -0500

    Update next.config.ts
    Quitar rewrite de diseno-de-sonrisa

--- next.config.ts
+++ next.config.ts
@@ -137,10 +137,6 @@ const nextConfig: NextConfig = {
 async rewrites() {
     return [
       {
-        source: '/diseno-de-sonrisa',
-        destination: 'https://sites.leadconnectorhq.com/preview/oS37NiFZQpFc66t5Ts4L',
-      },
-      {
         source: '/gracias-diseno-de-sonrisa',
         destination: 'https://sites.leadconnectorhq.com/preview/jt6T58ZsZdpn0fvKDh9n',
       },
